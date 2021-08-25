71af7f41e8c96ea57d5e504d84c51c96646550a4
GRAM\wp6543 <zerda@korea.ac.kr>
2021-08-26 04:29:30 +0900

    21.08.26. 소제목

--- creotest/src/components/Contact.js
+++ creotest/src/components/Contact.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
+import Title from './Title';
 
 export default class Contact extends Component {
     render() {
         return (
             <div>
+                <Title/>
                 <h6>크레오 컨택</h6>
                 Name : <input className="Name" type="text" alter="name"/>
                 E-mail : <input className="Email" type="text" alter="email"/>
---
+++ creotest/src/components/Title.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Button.scss';
+
+function Title() {
+  return <button className="Title">소제목</button>;
+}
+
+export default Title;
