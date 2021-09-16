c713bea0dfff015abf29b659e3be83a0b5d03336
zerda217 <zerda@korea.ac.kr>
2021-09-16 12:22:24 +0900

    html+css+js 반응형 메뉴/푸터 > react 로 수정해야 함

--- creotest/src/App.js
+++ creotest/src/App.js
@@ -6,6 +6,13 @@ import { Home, About, Creo, Works, Careers, Contact2 } from './components';
 import Button from './components/Button';
 
 const App = () => {
+//   const toggleBtn = document.querySelector('.nav_toggleBtn');
+//   const menu = document.querySelector('.nav');
+
+// toggleBtn.addEventListener('click', () => {
+// menu.classList.toggle('active');
+// });
+
   return (
     <div className="creo">
       <div className="body">
@@ -20,6 +27,7 @@ const App = () => {
             <li className="nav_li"><Link to="/components/Careers">CAREERS</Link></li>
             <li className="nav_li"><Link to="/components/Contact2">CONTACT</Link></li>         
           </ul>
+          <Link to="#" className="nav_toggleBtn">o</Link>
       </div>
         <div className="Main">
           <hr/>
@@ -37,6 +45,7 @@ const App = () => {
         <div className="FooterUp">
           Contact us with<br/>
           your challenge
+          <h6>크레오 코리아 관련 제품 및 기타 궁금하신 점을 해결해 드립니다.</h6>
         <div>
           <Link to = "./Contact2">
               <Button/>
@@ -44,15 +53,15 @@ const App = () => {
         </div>
       </div>
       <div className="FooterDown">
-        <img className="footerlogo" src="https://blog.creokorea.com//uploads/l_f77a7d016b.png" alt="logo"/>
+        <img className="FooterLogo" src="https://blog.creokorea.com//uploads/l_f77a7d016b.png" alt="logo"/>
           <span>
-              <li>대표 : 김홍범 / 서울특별시 성동구 연무장 5가길 7</li>
-              <li>TEL : 02-4321-432</li>
-              <li>개인정보취급방침</li>
-              <li>사업자등록번호 :</li>
-              <li>대표메일 : hb.kim@creokorea.com</li>          
+              <li className="Footer_li">대표 : 김홍범 / 서울특별시 성동구 연무장 5가길 7</li>
+              <li className="Footer_li">대표메일 : hb.kim@creokorea.com</li> 
+              <li className="Footer_li">사업자등록번호 : 123-456-789</li>
+              <li className="Footer_li">TEL : 02-4321-432</li>
+              <li className="Footer_li">개인정보취급방침</li>         
           </span>
-          <div className="copyright">© CREO KOREA all right reseved</div>
+          <div className="Copyright">© CREO KOREA all right reseved</div>
         </div>               
         </div>
       </div>
