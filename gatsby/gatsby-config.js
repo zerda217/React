cf10f69501e9c30eab76567d4b9d4f884a934d79
zerda217 <84703104+zerda217@users.noreply.github.com>
2021-10-12 18:44:42 +0900

    new gatsby

---
+++ gatsby/gatsby-config.js
@@ -0,0 +1,15 @@
+// gatsby-config.js
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {
+        // basePath defaults to `/`
+      },
+    },
+  ],
+  siteMetadata: {
+    title: `Hello, Gatsby!`,
+    social: {},
+  }
+}
