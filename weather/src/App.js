16c5a0686872669952306cb266387896c60e4502
zerda217 <zerda@korea.ac.kr>
2021-11-10 19:02:42 +0900

    layout 교체

--- weather/src/App.js
+++ weather/src/App.js
@@ -2,16 +2,9 @@ import React, { Component } from 'react';
 import { Layout, Menu } from 'antd';
 import './App.css';
 import 'antd/dist/antd.css';
-import {
-  MenuUnfoldOutlined,
-  MenuFoldOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
-  UploadOutlined,
-} from '@ant-design/icons';
-import calendar from './components/atoms/calendar';
+import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
 
-const { Header, Sider, Content } = Layout;
+const { Header, Content, Footer, Sider } = Layout;
 
 class App extends Component {
   state = {
@@ -28,40 +21,42 @@ class App extends Component {
     return (
       <div className='wrapper-main'>
         <Layout>
-          <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
-            <div className="logo" />
-            <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-              <Menu.Item key="1" icon={<UserOutlined />}>
-                nav 1
-            </Menu.Item>
-              <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-                nav 2
-            </Menu.Item>
-              <Menu.Item key="3" icon={<UploadOutlined />}>
-                nav 3
-            </Menu.Item>
-            </Menu>
-          </Sider>
-          <Layout className="site-layout">
-            <Header className="site-layout-background" style={{ padding: 0 }}>
-              {React.createElement(this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-                className: 'trigger',
-                onClick: this.toggle,
-              })}
-            </Header>
-            <Content
-              className="site-layout-background"
-              style={{
-                margin: '24px 16px',
-                padding: 24,
-              }}
-            >
-              contents
-              <calendar />
-
-          </Content>
-          </Layout>
-        </Layout>
+    <Sider
+      breakpoint="lg"
+      collapsedWidth="0"
+      onBreakpoint={broken => {
+        console.log(broken);
+      }}
+      onCollapse={(collapsed, type) => {
+        console.log(collapsed, type);
+      }}
+    >
+      <div className="logo" />
+      <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+        <Menu.Item key="1" icon={<UserOutlined />}>
+          nav 1
+        </Menu.Item>
+        <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+          nav 2
+        </Menu.Item>
+        <Menu.Item key="3" icon={<UploadOutlined />}>
+          nav 3
+        </Menu.Item>
+        <Menu.Item key="4" icon={<UserOutlined />}>
+          nav 4
+        </Menu.Item>
+      </Menu>
+    </Sider>
+    <Layout>
+      <Header className="site-layout-sub-header-background" style={{ padding: 0 }} />
+      <Content style={{ margin: '24px 16px 0' }}>
+        <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+          content
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+    </Layout>
+  </Layout>
       </div>
     );
   }
