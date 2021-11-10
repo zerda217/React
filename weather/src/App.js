0672ad320e9cdd2ba84deb0baa231f134f5089a4
zerda217 <zerda@korea.ac.kr>
2021-11-10 18:47:43 +0900

    layout 임시 적용

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,10 +1,70 @@
+import React, { Component } from 'react';
+import { Layout, Menu } from 'antd';
 import './App.css';
-import Index from './pages';
+import 'antd/dist/antd.css';
+import {
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+} from '@ant-design/icons';
+import calendar from './components/atoms/calendar';
 
-function App() {
-  return (
-    <Index />
-  );
+const { Header, Sider, Content } = Layout;
+
+class App extends Component {
+  state = {
+    collapsed: false,
+  };
+
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  };
+
+  render() {
+    return (
+      <div className='wrapper-main'>
+        <Layout>
+          <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
+            <div className="logo" />
+            <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+              <Menu.Item key="1" icon={<UserOutlined />}>
+                nav 1
+            </Menu.Item>
+              <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+                nav 2
+            </Menu.Item>
+              <Menu.Item key="3" icon={<UploadOutlined />}>
+                nav 3
+            </Menu.Item>
+            </Menu>
+          </Sider>
+          <Layout className="site-layout">
+            <Header className="site-layout-background" style={{ padding: 0 }}>
+              {React.createElement(this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+                className: 'trigger',
+                onClick: this.toggle,
+              })}
+            </Header>
+            <Content
+              className="site-layout-background"
+              style={{
+                margin: '24px 16px',
+                padding: 24,
+              }}
+            >
+              contents
+              <calendar />
+
+          </Content>
+          </Layout>
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
