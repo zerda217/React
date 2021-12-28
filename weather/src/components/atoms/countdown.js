9c07f89460d4a7d4c1fea606cf662f7f7ab9b3d0
zerda217 <zerda@korea.ac.kr>
2021-12-28 12:47:09 +0900

    노마드 3.9

--- weather/src/components/atoms/countdown.js
+++ weather/src/components/atoms/countdown.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 
-const Countdown = () => {
+const MinutesToHours = () => {
   const [amount, setAmount] = useState(0);
   const [inverted, setInverted] = useState(false);
   const onChange = (e) => {
@@ -14,7 +14,6 @@ const Countdown = () => {
 
   return (
     <div>
-      <h1>변환기</h1>
       <div>
         <label>Minutes :</label>
         <input 
@@ -41,6 +40,72 @@ const Countdown = () => {
         <button onClick={onInvert}>
           {inverted? "Turn back" : "Invert"}
         </button>
+    </div>
+  )
+}
+
+function KmToMiles () {
+  const [amount, setAmount] = useState(0);
+  const [inverted, setInverted] = useState(false);
+  const onChange = (e) => {
+    setAmount(e.target.value)
+  }
+  const reset = () => (setAmount(0));
+  const onInvert = () => {
+    reset();
+    setInverted((current) => (!current))
+  };
+
+  return (
+    <div>
+      <div>
+        <label>KiloMeters :</label>
+        <input 
+          value={inverted ? amount/0.621371 : amount}
+          placeholder='Minutes' 
+          type='number'
+          onChange={onChange}
+          disabled={inverted}
+          />
+        </div>
+        <h4>변환할 값 :{amount}</h4>
+
+      <div>
+        <label>Miles :</label>
+        <input 
+          value={inverted ? amount : amount * 0.621371}
+          placeholder='Hours' 
+          type='number'
+          onChange={onChange}
+          disabled={!inverted}
+          />
+      </div>
+        <button onClick={reset}>Reset</button>
+        <button onClick={onInvert}>
+          {inverted? "Turn back" : "Invert"}
+        </button>
+    </div>
+  )
+}
+
+const Countdown = () => {
+  const [index, setIndex] = useState("0")
+  const onSelect = (e) => {
+    setIndex(e.target.value)
+  }
+
+  return (
+    <div>
+      <h1>변환기</h1>
+      <select value={index} onChange={onSelect}>
+        <option value="xx">Select your units</option>
+        <option value="0">Minutes & Hours</option>
+        <option value="1">Km & Miles</option>
+      </select>
+      <hr />
+      {index === "xx" ? "please select" : null}
+      {index === "0" ? <MinutesToHours /> : null}
+      {index === "1" ? <KmToMiles /> : null}
     </div>
   )
 }
