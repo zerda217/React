61a41d96534c3928894c58e386e75510003cad01
zerda217 <zerda@korea.ac.kr>
2021-12-29 12:21:20 +0900

    ga-4-react 라이브러리 삭제

--- weather/src/index.js
+++ weather/src/index.js
@@ -4,29 +4,14 @@ import './index.css';
 import 'antd/dist/antd.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import GA4React, { useGA4React } from "ga-4-react";
 
-const ga4react = new GA4React('G-DLXW67L3KP')
-
-function MyApp() {
-  const ga = useGA4React();
-  console.log(ga);
-  return <></>
-}
-
-(async () => {
-  await ga4react.initialize();
+// const ga4react = new GA4React('G-DLXW67L3KP')
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <MyApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
-})();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
