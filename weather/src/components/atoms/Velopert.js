c3d40b4cd1e2ce82dec7d7f12ba3ea686941d531
zerda217 <zerda@korea.ac.kr>
2022-04-21 10:50:09 +0900

    useReducer

--- weather/src/components/atoms/Velopert.js
+++ weather/src/components/atoms/Velopert.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useCallback, useMemo } from 'react';
+import React, { useRef, useState, useMemo, useCallback, useReducer } from 'react';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
 
@@ -7,23 +7,12 @@ function countActiveUsers(users) {
   return users.filter(user => user.active).length;
 }
 
-const Velopert = () => {
-
-  const [inputs, setInputs] = useState({
+const initialState = {
+  inputs: {
     username: '',
     email: ''
-  });
-  const { username, email } = inputs;
-
-  const onChange = useCallback(e => {
-    const { name, value } = e.target;
-    setInputs(inputs => ({
-      ...inputs,
-      [name]: value
-    }));
-  }, []);
-
-  const [users, setUsers] = useState([
+  },
+  users: [
     {
       id: 1,
       username: 'velopert',
@@ -42,59 +31,95 @@ const Velopert = () => {
       email: 'liz@example.com',
       active: false
     }
-  ]);
+  ]
+};
 
+function reducer(state, action) {
+  switch (action.type) {
+    case 'CHANGE_INPUT':
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.name]: action.value
+        }
+      };
+    case 'CREATE_USER':
+      return {
+        inputs: initialState.inputs,
+        users: state.users.concat(action.user)
+      };
+    case 'TOGGLE_USER':
+      return {
+        ...state,
+        users: state.users.map(user => user.id === action.id ?
+          {...user, active: !user.active} : user)
+      };
+    case 'REMOVE_USER':
+      return {
+        ...state,
+        users: state.users.filter(user => user.id !== action.id)
+      };
+    default:
+      return state;
+  }
+}
+
+function App() {
+  const [ state, dispatch ] = useReducer(reducer, initialState);
   const nextId = useRef(4);
+  const { users } = state;
+  const { username, email } = state.inputs;
+
+  const onChange = useCallback(e => {
+    const { name, value } = e.target;
+    dispatch({
+      type: 'CHANGE_INPUT',
+      name,
+      value
+    });
+  }, []);
 
   const onCreate = useCallback(() => {
-    const user = {
-      id: nextId.current,
-      username,
-      email
-    };
-    setUsers(users => users.concat(user));
-    //setUsers([...users, user]);
-
-    setInputs({
-      username: '',
-      email: ''
+    dispatch({
+      type: 'CREATE_USER',
+      user: {
+        id: nextId.current,
+        username,
+        email
+      }
     });
     nextId.current += 1;
   }, [username, email]);
 
+  const onToggle = useCallback(id => {
+    dispatch({
+      type: 'TOGGLE_USER',
+      id
+    });
+  }, []);
+
   const onRemove = useCallback(id => {
-      // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
-      // = user.id 가 id 인 것을 제거함
-      setUsers(users => users.filter(user => user.id !== id));
-    },
-    []
-  );
+    dispatch({
+      type: 'REMOVE_USER',
+      id
+    });
+  }, []);
 
-  const onToggle = useCallback(
-    id => {
-      setUsers(users =>
-        users.map(user =>
-          user.id === id ? { ...user, active: !user.active } : user
-        )
-      );
-    },
-    []
-  );
-
-  const count = useMemo(() => countActiveUsers(users), [users] );
+  const count = useMemo(() => countActiveUsers(users), [users]);
 
   return (
-    <div>
-      <CreateUser
-        username={username}
-        email={email}
+    <>
+      <CreateUser 
+        username={username} 
+        email={email} 
         onChange={onChange}
         onCreate={onCreate}
       />
-      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
-        <div>활성사용자 수 : {count}</div>
-    </div>
-  )
+      <UserList users={users} onToggle={onToggle} onRemove={onRemove} />
+      <div>활성사용자 수 : {count} </div>
+    </>
+  );
 }
 
-export default Velopert
+export default App;
